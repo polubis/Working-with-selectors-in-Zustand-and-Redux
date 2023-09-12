3dc73081be5ec5115fc9db94b9cd24603d8a8399
Adrian Połubiński <polubis@wp.pl>
2023-09-12 08:52:30 +0200

    Update 17.ts

--- snippets/17.ts
+++ snippets/17.ts
@@ -1,7 +1,7 @@
 import { createSelector } from "reselect";
 
 const getCounter = (state: Store) => state.counter;
-// Memoized selector.
+
 const getFibonacci = createSelector([getCounter], (counter) =>
   counter > 30 ? "To big to calculate" : fibonacci(counter)
 );
